1273db88f8fa5d23698530b77875d757ac89a2a5
Luis Ramos <luisviegasr2+github@gmail.com>
2025-09-24 23:20:04 +0100

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./hamburguer-menu.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
     </div>
   );
 }
+
+const HamburguerMenu = () => {
+  return (
+    <div className="menu-btn">
+      <div className="menu-btn__burger"></div>
+    </div>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <HamburguerMenu />
+      <h1 className="app-name">Advanced Calculator</h1>
+    </nav>
+  );
+};
 
 export default App;
